352924a88575b22b0094842388bd021a3abadee1
IJTINK7 <ijtink7@mail.ru>
2023-04-24 16:12:47 +0300

    Creating arrays of tasks for different to-do lists. Changing title of second to-do list.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import './App.css'
 import {Todolist} from "./Todolist";
 
 function App() {
+	const tasks1 = [
+		{ id: 1, title: "HTML&CSS", isDone: true },
+		{ id: 2, title: "JS", isDone: true },
+		{ id: 3, title: "ReactJS", isDone: false }
+	]
+	const tasks2 = [
+		{ id: 1, title: "Titanic", isDone: true },
+		{ id: 2, title: "Terminator 2", isDone: true },
+		{ id: 3, title: "Jaws", isDone: false }
+	]
 	return (
 		<div className='App'>
 			<Todolist title = "What to learn"/>
-			<Todolist title = "Songs"/>
-			<Todolist title = "Books"/>
+			<Todolist title = "James Cameron's movies"/>
 		</div>
 	)
 }
