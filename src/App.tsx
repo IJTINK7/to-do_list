98b0239ecf26572c852b6cd5111b72cc5a041822
IJTINK7 <ijtink7@mail.ru>
2023-04-24 17:23:31 +0300

    Finishing of the "Todolist 01" lesson

--- src/App.tsx
+++ src/App.tsx
@@ -28,3 +28,36 @@ function App() {
 }
 
 export default App;
+
+// Todolist 01
+
+// import React from 'react'
+// import './App.css'
+// import {Todolist} from "./Todolist";
+//
+// function App() {
+// 	const tasks1 = [
+// 		{ id: 1, title: "HTML&CSS", isDone: true },
+// 		{ id: 2, title: "JS", isDone: true },
+// 		{ id: 3, title: "ReactJS", isDone: false }
+// 	]
+// 	const tasks2 = [
+// 		{ id: 1, title: "Titanic", isDone: true },
+// 		{ id: 2, title: "Terminator 2", isDone: true },
+// 		{ id: 3, title: "Jaws", isDone: false }
+// 	]
+// 	return (
+// 		<div className='App'>
+// 			<Todolist
+// 				title = "What to learn"
+// 				tasks={tasks1}
+// 			/>
+// 			<Todolist
+// 				title = "James Cameron's movies"
+// 				tasks ={tasks2}
+// 			/>
+// 		</div>
+// 	)
+// }
+//
+// export default App;
