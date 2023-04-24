6aace9253bfb887446e6ab5c4294d812c26ba74e
IJTINK7 <ijtink7@mail.ru>
2023-04-24 14:18:53 +0300

    Creating visual part of future To-do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className="App">
+			<h3>What to learn</h3>
+			<div>
+				<input type="text"/>
+				<button>+</button>
+			</div>
+			<ul>
+				<li>HTML&CSS</li>
+				<li>JS</li>
+				<li>React</li>
+			</ul>
+			<div>
+				<button>All</button>
+				<button>Active</button>
+				<button>Complete</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
