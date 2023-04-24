b7cb30831af7cc57caa65e207630d24974db86aa
IJTINK7 <ijtink7@mail.ru>
2023-04-24 14:41:26 +0300

    Refactoring the code

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,22 @@ import './App.css';
 const App = () => {
 	return (
 		<div className="App">
-			<h3>What to learn</h3>
 			<div>
-				<input type="text"/>
-				<button>+</button>
-			</div>
-			<ul>
-				<li> <input type="checkbox" checked={true}/>HTML&CSS</li>
-				<li> <input type="checkbox" checked={true}/>JS</li>
-				<li> <input type="checkbox" checked={false}/>React</li>
-			</ul>
-			<div>
-				<button>All</button>
-				<button>Active</button>
-				<button>Complete</button>
+				<h3>What to learn</h3>
+				<div>
+					<input type="text"/>
+					<button>+</button>
+				</div>
+				<ul>
+					<li> <input type="checkbox" checked={true}/><span>HTML&CSS</span></li>
+					<li> <input type="checkbox" checked={true}/><span>JS</span></li>
+					<li> <input type="checkbox" checked={false}/><span>React</span></li>
+				</ul>
+				<div>
+					<button>All</button>
+					<button>Active</button>
+					<button>Complete</button>
+				</div>
 			</div>
 		</div>
 	);
