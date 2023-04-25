c8a301384d776d80dfcff6f7aa8db78805c3a7f0
IJTINK7 <ijtink7@mail.ru>
2023-04-25 09:18:56 +0300

    Preparing to the "Todolist 05" lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,69 @@
-import React from 'react'
-import './App.css'
-import {Todolist} from "./Todolist";
+import React, {useState} from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+import {v1} from 'uuid';
+
+export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
-	const tasks1 = [
-		{ id: 1, title: "HTML&CSS", isDone: true },
-		{ id: 2, title: "JS", isDone: true },
-		{ id: 3, title: "ReactJS", isDone: false }
-	]
-	const tasks2 = [
-		{ id: 1, title: "Titanic", isDone: true },
-		{ id: 2, title: "Terminator 2", isDone: true },
-		{ id: 3, title: "Jaws", isDone: false }
-	]
+
+	let [tasks, setTasks] = useState([
+		{id: v1(), title: "HTML&CSS", isDone: true},
+		{id: v1(), title: "JS", isDone: true},
+		{id: v1(), title: "ReactJS", isDone: false},
+		{id: v1(), title: "Rest API", isDone: false},
+		{id: v1(), title: "GraphQL", isDone: false},
+	]);
+	let [filter, setFilter] = useState<FilterValuesType>("all");
+
+
+	function removeTask(id: string) {
+		let filteredTasks = tasks.filter(t => t.id !== id);
+		setTasks(filteredTasks);
+	}
+
+	function addTask(title: string) {
+		let task = {id: v1(), title: title, isDone: false};
+		let newTasks = [task, ...tasks];
+		setTasks(newTasks);
+	}
+
+	function changeStatus(taskId: string, isDone: boolean) {
+		let task = tasks.find(t => t.id === taskId);
+		if (task) {
+			task.isDone = isDone;
+		}
+
+		setTasks([...tasks]);
+	}
+
+
+	let tasksForTodolist = tasks;
+
+	if (filter === "active") {
+		tasksForTodolist = tasks.filter(t => !t.isDone);
+	}
+	if (filter === "completed") {
+		tasksForTodolist = tasks.filter(t => t.isDone);
+	}
+
+	function changeFilter(value: FilterValuesType) {
+		setFilter(value);
+	}
+
+
 	return (
-		<div className='App'>
-			<Todolist
-				title = "What to learn"
-				tasks={tasks1}
-			/>
-			<Todolist
-				title = "James Cameron's movies"
-				tasks ={tasks2}
+		<div className="App">
+			<Todolist title="What to learn"
+					  tasks={tasksForTodolist}
+					  removeTask={removeTask}
+					  changeFilter={changeFilter}
+					  addTask={addTask}
+					  changeTaskStatus={changeStatus}
+					  filter={filter}
 			/>
 		</div>
-	)
+	);
 }
 
 export default App;
