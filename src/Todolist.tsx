d81f3f0085a35027b4c7d879b1374f8e8a2f6283
IJTINK7 <ijtink7@mail.ru>
2023-05-02 18:20:05 +0300

    Copying input, button elements to AddItemForm.tsx component

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -54,15 +54,15 @@ export function Todolist(props: PropsType) {
 		<h3> {props.title}
 			<button onClick={removeTodolist}>x</button>
 		</h3>
-		<div>
-			<input value={title}
-				   onChange={onChangeHandler}
-				   onKeyPress={onKeyPressHandler}
-				   className={error ? "error" : ""}
-			/>
-			<button onClick={addTask}>+</button>
-			{error && <div className="error-message">{error}</div>}
-		</div>
+		{/*<div>*/}
+		{/*	<input value={title}*/}
+		{/*		   onChange={onChangeHandler}*/}
+		{/*		   onKeyPress={onKeyPressHandler}*/}
+		{/*		   className={error ? "error" : ""}*/}
+		{/*	/>*/}
+		{/*	<button onClick={addTask}>+</button>*/}
+		{/*	{error && <div className="error-message">{error}</div>}*/}
+		{/*</div>*/}
 		<ul>
 			{
 				props.tasks.map(t => {
