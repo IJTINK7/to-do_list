07396dfeb8fb7ed4ca8042dd7eddad1dfc47eed6
IJTINK7 <ijtink7@mail.ru>
2023-05-09 21:01:02 +0300

    Adding "AppBar" component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
 import {AddItemForm} from "./components/AddItemForm";
-import {Container} from "@mui/material";
+import {AppBar, Button, Container, IconButton, Toolbar, Typography} from "@mui/material";
+import {Menu} from "@mui/icons-material";
 
 export type FilterValuesType = "all" | "active" | "completed";
 
@@ -71,6 +72,17 @@ function App() {
 	}
 	return (
 		<div className="App">
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton edge="start" color="inherit" aria-label="menu">
+						<Menu/>
+					</IconButton>
+					<Typography variant="h6">
+						News
+					</Typography>
+					<Button color="inherit">Login</Button>
+				</Toolbar>
+			</AppBar>
 			<Container fixed>
 				<AddItemForm callBack={addNewTodolist}/>
 				{todolists.map(el=>{
