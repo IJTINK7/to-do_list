055b50489cda971acd175d8343a99a3d924ea292
IJTINK7 <ijtink7@mail.ru>
2023-05-31 15:40:55 +0300

    Finishing Todolist 07 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -643,3 +643,69 @@ export default App;
 // 		</div>
 // 	);
 // }
+
+// Todolist 07/2 + tasksReducer.ts + filterReducer.ts
+
+//import React, {useReducer} from 'react';
+// import './App.css';
+// import {Todolist} from './Todolist';
+// import { v1 } from 'uuid';
+// import {addTaskAC, removeTaskAC, tasksReducer} from "./reducers/tasksReducer";
+// import {changeFilterAC, filterReducer} from "./reducers/filterReducer";
+//
+// export type FilterValuesType = "all" | "active" | "completed";
+//
+// function App() {
+//
+// 	// let [tasks, setTasks] = useState<TaskType[]>([
+// 	// 	{ id: v1(), title: "HTML&CSS", isDone: true },
+// 	// 	{ id: v1(), title: "JS", isDone: true },
+// 	// 	{ id: v1(), title: "ReactJS", isDone: false },
+// 	// 	{ id: v1(), title: "Rest API", isDone: false },
+// 	// 	{ id: v1(), title: "GraphQL", isDone: false },
+// 	// ]);
+//
+// 	let [tasks, dispatchTasks] = useReducer(tasksReducer,[
+// 		{ id: v1(), title: "HTML&CSS", isDone: true },
+// 		{ id: v1(), title: "JS", isDone: true },
+// 		{ id: v1(), title: "ReactJS", isDone: false },
+// 		{ id: v1(), title: "Rest API", isDone: false },
+// 		{ id: v1(), title: "GraphQL", isDone: false },
+// 	]);
+//
+// 	function removeTask(id: string) {
+// 		dispatchTasks(removeTaskAC(id))
+// 	}
+//
+// 	function addTask(title: string) {
+// 		dispatchTasks(addTaskAC(title))
+// 	}
+//
+// 	// let [filter, setFilter] = useState<FilterValuesType>("all");
+// 	let [filter, dispatchFilter] = useReducer(filterReducer, "all");
+//
+// 	let tasksForTodolist = tasks;
+//
+// 	if (filter === "active") {
+// 		tasksForTodolist = tasks.filter(t => !t.isDone);
+// 	}
+// 	if (filter === "completed") {
+// 		tasksForTodolist = tasks.filter(t => t.isDone);
+// 	}
+//
+// 	function changeFilter(value: FilterValuesType) {
+// 		dispatchFilter(changeFilterAC(value));
+// 	}
+//
+// 	return (
+// 		<div className="App">
+// 			<Todolist title="What to learn"
+// 					  tasks={tasksForTodolist}
+// 					  removeTask={removeTask}
+// 					  changeFilter={changeFilter}
+// 					  addTask={addTask} />
+// 		</div>
+// 	);
+// }
+//
+// export default App;
