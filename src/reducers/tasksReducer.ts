7c8677aa590b1f9d079e6402e394f216ca574201
IJTINK7 <ijtink7@mail.ru>
2023-05-31 14:12:42 +0300

    Remove tasks via useReducer

--- src/reducers/tasksReducer.ts
+++ src/reducers/tasksReducer.ts
@@ -1,3 +1,33 @@
-export const  tasksReducer = (state: any, action:any) => {
+import {TaskType} from "../Todolist";
+export const tasksReducer = (state: TaskType[], action:GeneralType) => {
+	switch (action.type) {
+		case "REMOVE-TASK":{
+			return state.filter(el => el.id !== action.payload.id)
+		}
+		case "ADD-TASK":{
+			return state
+		}
+		default:
+			return state
+	}
+}
 
+type GeneralType = RemoveTaskACType | AddTaskACType;
+
+type RemoveTaskACType = ReturnType<typeof removeTaskAC>
+
+export const removeTaskAC = (id: string) => {
+	return {
+		type: "REMOVE-TASK",
+		payload: {
+			id
+		}
+	} as const
+}
+type AddTaskACType = ReturnType<typeof addTaskAC>
+
+export const addTaskAC = () => {
+	return {
+		type: "ADD-TASK"
+	} as const
 }
