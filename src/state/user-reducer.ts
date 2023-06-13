61e1d3a2d9e47ef1653789026c45d45225992387
IJTINK7 <ijtink7@mail.ru>
2023-06-13 18:21:11 +0300

    Finishing using user-reducer.ts and user-reducer.test.ts files

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -11,15 +11,15 @@ type ActionType = {
 // меня вызовут и дадут мне стейт (почти всегда объект)
 // и инструкцию (action, тоже объект)
 // согласно прописанному type в этом action (инструкции) я поменяю state
-export const userReducer = (state: StateType, action: ActionType) => {
+export const userReducer = (state: StateType, action: ActionType):StateType => {
 	switch (action.type) {
 		case 'INCREMENT-AGE':
-			state.age = state.age + 1
-			return state
+			return {...state, age: state.age + 1}
 		case 'INCREMENT-CHILDREN-COUNT':
-			state.childrenCount = state.childrenCount + 1
-			return state
+			return {...state, childrenCount: state.childrenCount + 1}
+		case 'CHANGE-NAME':
+			return {...state, name: action.newName}
 		default:
-			throw new Error('I don\'t understand this type')
+			throw new Error("I don't understand this type")
 	}
 }
