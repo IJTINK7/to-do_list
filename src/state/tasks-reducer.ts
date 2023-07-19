0c37428c2ff058a80cdab262a9887f07b34175de
IJTINK7 <ijtink7@mail.ru>
2023-07-19 19:46:20 +0300

    Changing reducer and test for 'ADD-TASK' action.type

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,19 +1,19 @@
 import {TasksStateType} from "../App";
+import {v1} from "uuid";
 
-export type SecondActionType = {
-    type: ''
-}
 export type RemoveTaskACType = ReturnType<typeof removeTaskAC>
+export type AddTaskACACType = ReturnType<typeof addTaskAC>
 
 
-type ActionsType = RemoveTaskACType | SecondActionType
+type ActionsType = RemoveTaskACType | AddTaskACACType
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
         case 'REMOVE-TASK':
             return {...state, [action.todolistId]: state[action.todolistId].filter(t => t.id !== action.taskId)}
-        case '':
-            return state
+        case 'ADD-TASK':
+            let task = {id: v1(), title: action.title, isDone: false};
+            return {...state,[action.todolistId]: [task, ...state[action.todolistId]]}
         default:
             throw new Error("I don't understand this type")
     }
@@ -22,6 +22,6 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
 export const removeTaskAC = (taskId: string,todolistId: string) => {
     return { type: 'REMOVE-TASK', todolistId, taskId} as const
 }
-export const secondAC = (title: string): SecondActionType => {
-    return { type: ''}
+export const addTaskAC = (title: string, todolistId: string) => {
+    return { type: 'ADD-TASK',title,todolistId} as const
 }
