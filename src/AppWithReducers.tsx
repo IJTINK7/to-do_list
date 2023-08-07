dd798348d53e164ac223f0b7e910e6ed58a82559
IJTINK7 <ijtink7@mail.ru>
2023-08-07 18:56:18 +0300

    Fixing "changeTaskTitle" function

--- src/AppWithReducers.tsx
+++ src/AppWithReducers.tsx
@@ -7,7 +7,7 @@ import AppBar from '@mui/material/AppBar/AppBar';
 import {Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@mui/material";
 import {Menu} from "@mui/icons-material";
 import {todolistsReducer} from "./state/todolists-reducer";
-import {addTaskAC, removeTaskAC, tasksReducer} from "./state/tasks-reducer";
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from "./state/tasks-reducer";
 
 
 export type FilterValuesType = "all" | "active" | "completed";
@@ -54,29 +54,13 @@ function AppWithReducers() {
 	}
 
 	function changeStatus(id: string, isDone: boolean, todolistId: string) {
-		//достанем нужный массив по todolistId:
-		let todolistTasks = tasks[todolistId];
-		// найдём нужную таску:
-		let task = todolistTasks.find(t => t.id === id);
-		//изменим таску, если она нашлась
-		if (task) {
-			task.isDone = isDone;
-			// засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-			setTasks({...tasks});
-		}
+		const action = changeTaskStatusAC(id, isDone, todolistId)
+		dispatchToTasksReducer(action)
 	}
 
 	function changeTaskTitle(id: string, newTitle: string, todolistId: string) {
-		//достанем нужный массив по todolistId:
-		let todolistTasks = tasks[todolistId];
-		// найдём нужную таску:
-		let task = todolistTasks.find(t => t.id === id);
-		//изменим таску, если она нашлась
-		if (task) {
-			task.title = newTitle;
-			// засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-			setTasks({...tasks});
-		}
+		const action = changeTaskTitleAC(id, newTitle, todolistId)
+		dispatchToTasksReducer(action)
 	}
 
 	function changeFilter(value: FilterValuesType, todolistId: string) {
