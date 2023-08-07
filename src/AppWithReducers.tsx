3999e6720ffa33f80eac6248b185347d507bcfbc
IJTINK7 <ijtink7@mail.ru>
2023-08-07 18:52:11 +0300

    Changing "useState" hook to "useReducer" hook

--- src/AppWithReducers.tsx
+++ src/AppWithReducers.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
@@ -6,6 +6,8 @@ import {AddItemForm} from './AddItemForm';
 import AppBar from '@mui/material/AppBar/AppBar';
 import {Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@mui/material";
 import {Menu} from "@mui/icons-material";
+import {todolistsReducer} from "./state/todolists-reducer";
+import {tasksReducer} from "./state/tasks-reducer";
 
 
 export type FilterValuesType = "all" | "active" | "completed";
@@ -24,12 +26,12 @@ function AppWithReducers() {
 	let todolistId1 = v1();
 	let todolistId2 = v1();
 
-	let [todolists, setTodolists] = useState<Array<TodolistType>>([
+	let [todolists, dispatchToTodolistsReducer] = useReducer(todolistsReducer, [
 		{id: todolistId1, title: "What to learn", filter: "all"},
 		{id: todolistId2, title: "What to buy", filter: "all"}
 	])
 
-	let [tasks, setTasks] = useState<TasksStateType>({
+	let [tasks, dispatchToTasksReducer] = useReducer(tasksReducer, {
 		[todolistId1]: [
 			{id: v1(), title: "HTML&CSS", isDone: true},
 			{id: v1(), title: "JS", isDone: true}
