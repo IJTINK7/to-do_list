18bea19ca0506c8d19f18385b6b3b244478577eb
IJTINK7 <ijtink7@mail.ru>
2024-01-24 21:09:33 +0300

    Showing of difference between pictures via fallen tests

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -3,7 +3,7 @@ describe('addItemForm', () => {
         // APIs from jest-puppeteer
         // eslint-disable-next-line no-undef
         await page.goto('http://localhost:6006/iframe.html?args=&id=example-additemform--add-item-form-story&viewMode=story',
-            {waitUntil: "networkidle2", timeout: 10000});
+            {waitUntil: "networkidle2", timeout: 70000});
 
         // eslint-disable-next-line no-undef
         const image = await page.screenshot();
