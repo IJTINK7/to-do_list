6163e4adfc49de770c9f5f8a8207e32e7ed1b332
IJTINK7 <ijtink7@mail.ru>
2024-01-24 20:30:43 +0300

    Creating ReduxStoreProviderDecorator.tsx

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import AppWithRedux from "../AppWithRedux";
-import {Provider} from "react-redux";
-import {store} from "../state/store";
+import {ReduxStoreProviderDecorator} from "./ReduxStoreProviderDecorator";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta <typeof AppWithRedux> = {
@@ -13,6 +12,7 @@ const meta: Meta <typeof AppWithRedux> = {
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
+  decorators: [ReduxStoreProviderDecorator]
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
 } satisfies Meta<typeof AppWithRedux>;
 
@@ -20,6 +20,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const AppWithReduxStory: Story = {
-    render: () => <Provider store={store}><AppWithRedux/></Provider>
+    render: () => <AppWithRedux/>
 }
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+
