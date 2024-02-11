d204e71e826706a5cf1b7fc0afbcafa512b2f018
IJTINK7 <ijtink7@mail.ru>
2024-02-11 20:22:58 +0300

    Changing project structure

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import {Provider} from 'react-redux';
-import {store} from './state/store';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <Provider store={store}>
     <App />
-    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,7 +1,11 @@
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
 	title: 'API'
+}
+const settings = {
+	withCredentials: true
 }
 
 export const GetTodolists = () => {
@@ -9,29 +13,35 @@ export const GetTodolists = () => {
 	useEffect(() => {
 		// здесь мы будем делать запрос и ответ закидывать в стейт.
 		// который в виде строки будем отображать в div-ке
+		let promise = axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
+			.then((res)=>{
+				debugger
+				setState(res.data)
+			})
+		console.log(promise)
 
 	}, [])
 	return <div>{JSON.stringify(state)}</div>
 }
-export const CreateTodolist = () => {
-	const [state, setState] = useState<any>(null)
-	useEffect(() => {
-	}, [])
-
-	return <div>{JSON.stringify(state)}</div>
-}
-export const DeleteTodolist = () => {
-	const [state, setState] = useState<any>(null)
-	useEffect(() => {
-	}, [])
-
-	return <div>{JSON.stringify(state)}</div>
-}
-export const UpdateTodolistTitle = () => {
-	const [state, setState] = useState<any>(null)
-	useEffect(() => {
-	}, [])
-
-	return <div>{JSON.stringify(state)}</div>
-}
+// export const CreateTodolist = () => {
+// 	const [state, setState] = useState<any>(null)
+// 	useEffect(() => {
+// 	}, [])
+//
+// 	return <div>{JSON.stringify(state)}</div>
+// }
+// export const DeleteTodolist = () => {
+// 	const [state, setState] = useState<any>(null)
+// 	useEffect(() => {
+// 	}, [])
+//
+// 	return <div>{JSON.stringify(state)}</div>
+// }
+// export const UpdateTodolistTitle = () => {
+// 	const [state, setState] = useState<any>(null)
+// 	useEffect(() => {
+// 	}, [])
+//
+// 	return <div>{JSON.stringify(state)}</div>
+// }
 
