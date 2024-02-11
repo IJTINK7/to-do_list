46264966851576e5905557cf846290808c7a0f36
IJTINK7 <ijtink7@mail.ru>
2024-02-11 18:35:49 +0300

    Preparing for the 13th lesson

--- src/store.ts
+++
@@ -1,9 +0,0 @@
-import {combineReducers, legacy_createStore} from "redux";
-import {counterReducer} from "./counterReducer";
-
-let rootReducer = combineReducers({
-	superCounter: counterReducer
-})
-export type AppRootStateType = ReturnType<typeof rootReducer>
-
-export let store = legacy_createStore(rootReducer)
