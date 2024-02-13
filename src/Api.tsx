ccdf2995dfab2bb246c6411c496b1037e8693159
IJTINK7 <ijtink7@mail.ru>
2024-02-13 03:14:12 +0300

    Creating "taskApi" object with corresponding components. Adding typification for "get", "post","put","delete" methods for "taskApi" object.

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {todolistApi} from "./api/todolists-api";
+import {taskApi, todolistApi} from "./api/todolists-api";
 
 
 export const Api = () => {
@@ -9,6 +9,10 @@ export const Api = () => {
 			<CreateTodolist/>
 			<UpdateTodolistTitle/>
 			<DeleteTodolist/>
+			<GetTasks/>
+			<CreateTask/>
+			<UpdateTaskTitle/>
+			<DeleteTask/>
 		</div>
 	);
 };
@@ -50,6 +54,55 @@ export const DeleteTodolist = () => {
 	useEffect(() => {
 		let todolistId = "4a3dd011-08b5-43e4-bcc2-798b1c51dfe1"
 		todolistApi.deleteTodolist(todolistId)
+			.then((res)=>{
+				setState(res.data)
+			})
+	}, [])
+	return <div>{JSON.stringify(state)}</div>
+}
+
+
+let todolistId = "5cb9cb26-3bcb-49c2-ac5e-8c072965f516"
+
+export const GetTasks = () => {
+	const [state, setState] = useState<any>(null)
+	useEffect(() => {
+		taskApi.getTasks(todolistId)
+			.then((res)=>{
+				setState(res.data)
+				console.log(res.data)
+			})
+	}, [])
+	return <div>{JSON.stringify(state)}</div>
+}
+export const CreateTask = () => {
+	const [state, setState] = useState<any>(null)
+	useEffect(() => {
+		taskApi.createTask(todolistId,"My Task")
+			.then((res)=>{
+				setState(res.data)
+			})
+	}, [])
+
+	return <div>{JSON.stringify(state)}</div>
+}
+export const UpdateTaskTitle = () => {
+	const [state, setState] = useState<any>(null)
+	useEffect(() => {
+		let taskId = "70245a08-48e8-4dd1-99d0-d5d97d4bf143"
+		taskApi.updateTask(todolistId, taskId,"NEW TASK")
+			.then((res)=>{
+				setState(res.data)
+			})
+	}, [])
+
+	return <div>{JSON.stringify(state)}</div>
+}
+export const DeleteTask = () => {
+	const [state, setState] = useState<any>(null)
+	useEffect(() => {
+		let taskId = "2d9824d5-2b6f-4688-9af3-d48bc6c2a953"
+		taskApi.deleteTask(todolistId, taskId)
 			.then((res)=>{
 				setState(res.data)
 			})
--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export type TodolistType ={
+export type TodolistType = {
 	id: string
 	title: string
 	addedDate: string
@@ -25,6 +25,28 @@ export type ResponseType<D> = {
 	fieldsErrors: string[]
 	data: D
 }
+
+export type TaskType = {
+	addedDate: string
+	deadline: null
+	description:null
+	id: string
+	order: number
+	priority:number
+	startDate: null
+	status: number
+	title:string
+	todoListId:string
+}
+
+export type ResponseTaskDataType = {
+	resultCode: number
+	messages: string[],
+	data: {}
+}
+
+
+
 const instance = axios.create({
 	baseURL: "https://social-network.samuraijs.com/api/1.1",
 	withCredentials: true,
@@ -37,12 +59,27 @@ export const todolistApi = {
 		return instance.get<TodolistType[]>("/todo-lists")
 	},
 	createTodolist(title: string) {
-		return instance.post<ResponseType<CreateTodolistDataResponseType>>("/todo-lists",{title: title})
+		return instance.post<ResponseType<CreateTodolistDataResponseType>>("/todo-lists", {title: title})
 	},
 	updateTodolist(todolistId: string, title: string) {
-		return instance.put<ResponseType<UpdateTodolistDataResponseType>>(`/todo-lists/${todolistId}`,{title: title})
+		return instance.put<ResponseType<UpdateTodolistDataResponseType>>(`/todo-lists/${todolistId}`, {title: title})
 	},
 	deleteTodolist(todolistId: string) {
 		return instance.delete<ResponseType<DeleteTodolistDataResponseType>>(`/todo-lists/${todolistId}`)
+	},
+}
+
+export const taskApi = {
+	getTasks(todolistId: string) {
+		return instance.get<TaskType[]>(`/todo-lists/${todolistId}/tasks`)
+	},
+	createTask(todolistId: string, title: string) {
+		return instance.post<ResponseTaskDataType>(`/todo-lists/${todolistId}/tasks`, {title: title})
+	},
+	updateTask(todolistId: string, taskId: string, title: string) {
+		return instance.put<ResponseTaskDataType>(`/todo-lists/${todolistId}/tasks/${taskId}`, {title: title})
+	},
+	deleteTask(todolistId: string, taskId: string,) {
+		return instance.delete<ResponseTaskDataType>(`/todo-lists/${todolistId}/tasks/${taskId}`)
 	},
 }
